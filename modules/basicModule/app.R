b2f9884bded970c2c559ac18ca79da2fef925f85
TeroJii <78660782+TeroJii@users.noreply.github.com>
2023-08-30 13:55:53 +0300

    add module implementation of the simple histogram app

--- modules/basicModule/app.R
+++ modules/basicModule/app.R
@@ -9,17 +9,58 @@
 
 library(shiny)
 
-ui <- fluidPage(
-  selectInput("var", "Variable", names(mtcars)),
-  numericInput("bins", "bins", 10, min = 1),
-  plotOutput("hist")
-)
-server <- function(input, output, session) {
-  data <- reactive(mtcars[[input$var]])
-  output$hist <- renderPlot({
-    hist(data(), breaks = input$bins, main = input$var)
-  }, res = 96)
+# ####
+# # Non-module way ----
+# ####
+#
+# ui <- fluidPage(
+#   selectInput("var", "Variable", names(mtcars)),
+#   numericInput("bins", "bins", 10, min = 1),
+#   plotOutput("hist")
+# )
+#
+#
+# server <- function(input, output, session) {
+#   data <- reactive(mtcars[[input$var]])
+#   output$hist <- renderPlot({
+#     hist(data(), breaks = input$bins, main = input$var)
+#   }, res = 96)
+# }
+#
+# # Run the application
+# shinyApp(ui = ui, server = server)
+
+
+####
+# Module way ----
+####
+
+histogramUI <- function(id) {
+  tagList(
+    selectInput(NS(id, "var"), "Variable", choices = names(mtcars)),
+    numericInput(NS(id, "bins"), "bins", value = 10, min = 1),
+    plotOutput(NS(id, "hist"))
+  )
+}
+
+histogramServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    data <- reactive(mtcars[[input$var]])
+    output$hist <- renderPlot({
+      hist(data(), breaks = input$bins, main = input$var)
+    }, res = 96)
+  })
 }
 
 # Run the application
-shinyApp(ui = ui, server = server)
+histogramApp <- function() {
+  ui <- fluidPage(
+    histogramUI("hist1")
+  )
+  server <- function(input, output, session) {
+    histogramServer("hist1")
+  }
+  shinyApp(ui, server)
+}
+
+histogramApp()
